52aabf2ea8abdc95333458853c8416e9cfb88af9
Rafal Kluszczynski <klusi@icm.edu.pl>
2016-03-05 23:56:59 +0100

    creates unicoreSites bean

--- unicore-spring-boot-starter/src/main/java/pl/edu/icm/unicore/spring/central/tss/UnicoreSites.java
+++ unicore-spring-boot-starter/src/main/java/pl/edu/icm/unicore/spring/central/tss/UnicoreSites.java
@@ -4,9 +4,10 @@ import de.fzj.unicore.uas.TargetSystemFactory;
 import de.fzj.unicore.wsrflite.xmlbeans.client.RegistryClient;
 import eu.unicore.security.etd.TrustDelegation;
 import eu.unicore.util.httpclient.IClientConfiguration;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 import org.w3.x2005.x08.addressing.EndpointReferenceType;
 import pl.edu.icm.unicore.spring.UnicoreProperties;
 import pl.edu.icm.unicore.spring.security.GridClientHelper;
@@ -14,13 +15,13 @@ import pl.edu.icm.unicore.spring.security.GridClientHelper;
 import java.util.List;
 import java.util.stream.Collectors;
 
-//@Repository
-public class UnicoreSite {
+@Repository
+public class UnicoreSites {
     private final UnicoreProperties gridConfig;
     private final GridClientHelper clientHelper;
 
     @Autowired
-    public UnicoreSite(UnicoreProperties gridConfig, GridClientHelper clientHelper) {
+    public UnicoreSites(UnicoreProperties gridConfig, GridClientHelper clientHelper) {
         this.gridConfig = gridConfig;
         this.clientHelper = clientHelper;
     }
@@ -38,14 +39,14 @@ public class UnicoreSite {
             return new RegistryClient(registryEpr, clientConfiguration)
                     .listAccessibleServices(TargetSystemFactory.TSF_PORT)
                     .parallelStream()
-                    .map(endpointReferenceType -> new UnicoreSiteEntity(endpointReferenceType))
+                    .map(UnicoreSiteEntity::new)
                     .collect(Collectors.toList());
         } catch (Exception e) {
-            log.error(String.format("Error retrieving Target System from UNICORE Registry <%s>!", registryUrl), e);
+            logger.error(String.format("Error retrieving Target System from UNICORE Registry <%s>!", registryUrl), e);
             // TODO: should be used RuntimeException?
             throw new UnavailableSiteException(e);
         }
     }
 
-    private Log log = LogFactory.getLog(UnicoreSite.class);
+    private static final Logger logger = LoggerFactory.getLogger(UnicoreSites.class);
 }
